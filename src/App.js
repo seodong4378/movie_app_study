4ada460b65121a999b740442ded3bf2f2dcbc8ed
seodong4378 <seodong4378@naver.com>
2021-07-18 23:13:54 +0900

    App Calss and state control

--- src/App.js
+++ src/App.js
@@ -1,55 +1,29 @@
-import React, { Fragment } from "react";
+import React, {Component} from "react";
+import PropTypes from 'prop-types';
 
-const foodILike = [
-  {
-    id : 1,
-    name: "Kimchi",
-    image:
-      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
-  },
-  {
-    id : 2,
-    name: "Samgyeopsal",
-    image:
-      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg"
-  },
-  {
-    id : 3,
-    name: "Bibimbap",
-    image:
-      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb"
-  },
-  {
-    id : 4,
-    name: "Doncasu",
-    image:
-      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg"
-  },
-  {
-    id : 5,
-    name: "Kimbap",
-    image:
-      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg"
-  }
-];
+class App extends Component {
+  state = {
+    num : 0
+  };
 
-function Food({favorite, picture}) {
-  return (
+  //plus() { this.setState({num:1})} not working
+  plus = () =>{
+    this.setState(current => ({num : current.num +1}));
+  };
+
+  minus = () => {
+    this.setState(current => ({num : current.num -1}));
+  };
+
+  render() {
+    return (
     <div>
-      <h2>I like {favorite}.</h2>
-      <img src={picture} alt ={favorite}></img>
+      <h1>Number is {this.state.num}</h1>
+      <button onClick={this.plus}>Plus</button>
+      <button onClick={this.minus}>Minus</button>
     </div>
-  )
-}
-
-function App() {
-  return (
-    <div className="App">
-      {foodILike.map((item) => 
-        <Food favorite={item.name} picture={item.image} key={item.id}/>
-        )}
-    </div>
-  );
+    );
+  };
 }
 
 export default App;
