693f32755f08214f3050c4b55abfe2a66d9a09a8
seodong4378 <seodong4378@naver.com>
2021-07-20 18:07:26 +0900

    Rendering movie DB API

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 import React, {Component} from "react";
-import PropTypes from 'prop-types';
+import axios from "axios";
+import Movie from "./Movie";
 
 class App extends Component {
   state = {
-    isLoading : true
+    isLoading : true,
+    movies : []
   }
 
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({isLoading : false});
-    }, 5000);
+  getMovies = async () =>  {
+    const movieFromDb = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json");
+    console.log(movieFromDb, "start getMovies");
+    this.setState({ movies :movieFromDb.data.data.movies , isLoading: false });
+  }
+
+  componentDidMount () {
+    this.getMovies();
   }
  
   render() {
+    const { isLoading, movies } = this.state;
     return (
-    <div>
-      <h1>{this.state.isLoading ? "Loading..." : "I'm ready"}</h1>
-    </div>
+      <div>
+        {isLoading
+          ? "Loading..."
+          : movies.map(movie => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+              />
+              ))}
+      </div>
     );
-  };
+  }
 }
 
 export default App;
